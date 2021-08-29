2c438eb35cdc4fee5d59b7197c54e025aea4c952
Devesh Pal <newdev0@outlook.com>
2021-08-29 15:50:02 +0530

    Update rpcbaseerrors.py

--- telethon/errors/rpcbaseerrors.py
+++ telethon/errors/rpcbaseerrors.py
@@ -1,15 +1,23 @@
+from ..tl import functions
+
+_NESTS_QUERY = (
+    functions.InvokeAfterMsgRequest,
+    functions.InvokeAfterMsgsRequest,
+    functions.InitConnectionRequest,
+    functions.InvokeWithLayerRequest,
+    functions.InvokeWithoutUpdatesRequest,
+    functions.InvokeWithMessagesRangeRequest,
+    functions.InvokeWithTakeoutRequest,
+)
+
 class RPCError(Exception):
     """Base class for all Remote Procedure Call errors."""
-
     code = None
     message = None
 
     def __init__(self, request, message, code=None):
-        super().__init__(
-            "RPCError {}: {}{}".format(
-                code or self.code, message, self._fmt_request(request)
-            )
-        )
+        super().__init__('RPCError {}: {}{}'.format(
+            code or self.code, message, self._fmt_request(request)))
 
         self.request = request
         self.code = code
@@ -17,7 +25,15 @@ class RPCError(Exception):
 
     @staticmethod
     def _fmt_request(request):
-        return " (caused by {})".format(request.__class__.__name__)
+        n = 0
+        reason = ''
+        while isinstance(request, _NESTS_QUERY):
+            n += 1
+            reason += request.__class__.__name__ + '('
+            request = request.query
+        reason += request.__class__.__name__ + ')' * n
+
+        return ' (caused by {})'.format(reason)
 
     def __reduce__(self):
         return type(self), (self.request, self.message, self.code)
@@ -27,9 +43,8 @@ class InvalidDCError(RPCError):
     """
     The request must be repeated, but directed to a different data center.
     """
-
     code = 303
-    message = "ERROR_SEE_OTHER"
+    message = 'ERROR_SEE_OTHER'
 
 
 class BadRequestError(RPCError):
@@ -38,9 +53,8 @@ class BadRequestError(RPCError):
     using a form and contains user generated data, the user should be
     notified that the data must be corrected before the query is repeated.
     """
-
     code = 400
-    message = "BAD_REQUEST"
+    message = 'BAD_REQUEST'
 
 
 class UnauthorizedError(RPCError):
@@ -48,9 +62,8 @@ class UnauthorizedError(RPCError):
     There was an unauthorized attempt to use functionality available only
     to authorized users.
     """
-
     code = 401
-    message = "UNAUTHORIZED"
+    message = 'UNAUTHORIZED'
 
 
 class ForbiddenError(RPCError):
@@ -58,18 +71,16 @@ class ForbiddenError(RPCError):
     Privacy violation. For example, an attempt to write a message to
     someone who has blacklisted the current user.
     """
-
     code = 403
-    message = "FORBIDDEN"
+    message = 'FORBIDDEN'
 
 
 class NotFoundError(RPCError):
     """
     An attempt to invoke a non-existent object, such as a method.
     """
-
     code = 404
-    message = "NOT_FOUND"
+    message = 'NOT_FOUND'
 
 
 class AuthKeyError(RPCError):
@@ -77,9 +88,8 @@ class AuthKeyError(RPCError):
     Errors related to invalid authorization key, like
     AUTH_KEY_DUPLICATED which can cause the connection to fail.
     """
-
     code = 406
-    message = "AUTH_KEY"
+    message = 'AUTH_KEY'
 
 
 class FloodError(RPCError):
@@ -89,9 +99,8 @@ class FloodError(RPCError):
     attempt to request a large number of text messages (SMS) for the same
     phone number.
     """
-
     code = 420
-    message = "FLOOD"
+    message = 'FLOOD'
 
 
 class ServerError(RPCError):
@@ -100,9 +109,8 @@ class ServerError(RPCError):
     for example, there was a disruption while accessing a database or file
     storage.
     """
-
     code = 500  # Also witnessed as -500
-    message = "INTERNAL"
+    message = 'INTERNAL'
 
 
 class TimedOutError(RPCError):
@@ -110,25 +118,14 @@ class TimedOutError(RPCError):
     Clicking the inline buttons of bots that never (or take to long to)
     call ``answerCallbackQuery`` will result in this "special" RPCError.
     """
-
     code = 503  # Only witnessed as -503
-    message = "Timeout"
+    message = 'Timeout'
 
 
 BotTimeout = TimedOutError
 
 
-base_errors = {
-    x.code: x
-    for x in (
-        InvalidDCError,
-        BadRequestError,
-        UnauthorizedError,
-        ForbiddenError,
-        NotFoundError,
-        AuthKeyError,
-        FloodError,
-        ServerError,
-        TimedOutError,
-    )
-}
+base_errors = {x.code: x for x in (
+    InvalidDCError, BadRequestError, UnauthorizedError, ForbiddenError,
+    NotFoundError, AuthKeyError, FloodError, ServerError, TimedOutError
+)}
